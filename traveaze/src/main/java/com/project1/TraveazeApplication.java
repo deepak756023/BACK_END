7e2207fbc1e7cc437d3b9694f5c83ecc4610cca2
deepak756023 <deepaknayak55260@gmail.com>
2025-01-18 08:11:59 +0530

    folder created for backend operations

---
+++ traveaze/src/main/java/com/project1/TraveazeApplication.java
@@ -0,0 +1,13 @@
+package com.project1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraveazeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraveazeApplication.class, args);
+	}
+
+}
